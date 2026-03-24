f4d07422628f2f71476b971a6f37679c9259f164
Проворова Анастасия ТКИ-142 <ap37ru@icloud.com>
2026-03-24 14:50:58 +0300

    Update Plane.cpp

--- Plane.cpp
+++ Plane.cpp
@@ -48,3 +48,13 @@ double Plane::distanceToPoint(const Point& p) const
 
     return numerator / denominator;
 }
+
+bool Point::operator == (const Point& other) const 
+{
+    return (this->x == other.x) && (this->y == other.y);
+}
+
+bool Point::operator != (const Point& other) const 
+{
+    return !(*this == other);
+}
