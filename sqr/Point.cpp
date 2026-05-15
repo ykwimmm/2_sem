4252ed14334e7c2e2acec6cb9c7db8872b751c42
Проворова Анастасия ТКИ-142 <ap37ru@icloud.com>
2026-05-15 22:54:08 +0300

    Update Point.cpp

--- sqr/Point.cpp
+++ sqr/Point.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-Point::Point(double x, double y, double z): m_x(x), m_y(y), m_z(z) 
+Point::Point(const double x, const double y, const double z): m_x(x), m_y(y), m_z(z) 
 {
     
 }
