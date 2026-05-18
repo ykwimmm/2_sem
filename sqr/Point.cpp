c2690fa5513c742ee11d5d1a48f99f93738a057b
Проворова Анастасия ТКИ-142 <ap37ru@icloud.com>
2026-05-18 12:28:16 +0300

    Update Point.cpp

--- sqr/Point.cpp
+++ sqr/Point.cpp
@@ -37,52 +37,6 @@ bool Point::operator!=(const Point& other) const
     return !(*this == other);
 }
 
-bool Point::operator<(const Point& other) const 
-{
-    if (fabs(m_x - other.m_x) >= numeric_limits<double>::epsilon()) {
-        return m_x < other.m_x;
-    }
-    if (fabs(m_y - other.m_y) >= numeric_limits<double>::epsilon()) {
-        return m_y < other.m_y;
-    }
-    return m_z < other.m_z - numeric_limits<double>::epsilon();
-}
-
-bool Point::operator<=(const Point& other) const 
-{
-    return (*this < other) || (*this == other);
-}
-
-bool Point::operator>(const Point& other) const 
-{
-    return !(*this <= other);
-}
-
-bool Point::operator>=(const Point& other) const 
-{
-    return !(*this < other);
-}
-
-Point Point::operator+(double val) const 
-{
-    return Point(m_x + val, m_y + val, m_z + val);
-}
-
-Point Point::operator-(double val) const 
-{
-    return Point(m_x - val, m_y - val, m_z - val);
-}
-
-Point Point::operator+(const Point& other) const 
-{
-    return Point(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z);
-}
-
-Point Point::operator-(const Point& other) const 
-{
-    return Point(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z);
-}
-
 std::ostream& operator<<(std::ostream& os, const Point& p) 
 {
     os << "(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ")";
